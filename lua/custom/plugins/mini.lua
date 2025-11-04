c85e3a42703c65130225487fe87fd208979a9eb1
Reece Yankey <ryankey@mitre.org>
2025-11-04 07:06:05 -0500

    open minifiles on current dir/file

--- lua/custom/plugins/mini.lua
+++ lua/custom/plugins/mini.lua
@@ -2,7 +2,13 @@ return { -- Collection of various small independent plugins/modules
   'echasnovski/mini.nvim',
   config = function()
     require('mini.files').setup()
-    vim.keymap.set('n', '<leader>.', MiniFiles.open, { desc = 'Open MiniFiles' })
+
+    vim.keymap.set('n', '<leader>.', function()
+      local buf_name = vim.api.nvim_buf_get_name(0)
+      local path = vim.fn.filereadable(buf_name) == 1 and buf_name or vim.fn.getcwd()
+      MiniFiles.open(path)
+      MiniFiles.reveal_cwd()
+    end, { desc = 'Open MiniFiles' })
 
     -- Better Around/Inside textobjects
     --
